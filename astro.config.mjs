24605a0f68edf8045993be7943d8e1d8fa340d8b
Laween Al Sulaivany <78901349+laween-alsulaivany@users.noreply.github.com>
2026-09-11 10:06:33 -0500

    changed site URL to new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://laween-alsulaivany.github.io',
+  site: 'https://laween.dev',
   output: 'static',
 });
