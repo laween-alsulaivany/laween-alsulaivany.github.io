4ba2dd94e6b88bafd9ab5d9cc0e8dc8af2445a06
Laween Al Sulaivany <78901349+laween-alsulaivany@users.noreply.github.com>
2026-09-10 22:31:44 -0500

    add portfolio home page

---
+++ src/data/profile.ts
@@ -0,0 +1,11 @@
+export const profile = {
+  name: 'Laween Al-Sulaivany',
+  role: 'Software Developer',
+  github: 'https://github.com/laween-alsulaivany',
+  linkedin: 'https://www.linkedin.com/in/laweenhamza/',
+  about: [
+    "I make a lot of different things, usually because something catches my attention and I want to see how far I can take it. Some projects start as little tools I want for myself, while others grow into software that people actually use. Occasionally, it turns into something much stranger, like teaching an agent to fight a Dark Souls boss.",
+
+    "I enjoy the part between getting something to work and getting it to work right. I tend to get pulled into the details like figuring out why something fails, cleaning up rough edges, and changing things until the result actually feels good to use. I studied civil engineering before computer science, so maybe I've always just liked building things."
+  ],
+} as const;
