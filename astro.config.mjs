a162765dc16a22fe83921786e3d471711e8c6529
Laween Al Sulaivany <78901349+laween-alsulaivany@users.noreply.github.com>
2026-09-11 11:04:42 -0500

    added sitemap integration and robots.txt file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
 
 export default defineConfig({
   site: 'https://laween.dev',
   output: 'static',
+  integrations: [sitemap()],
 });
